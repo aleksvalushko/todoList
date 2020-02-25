5751ed809d2468da7b1eab79f482b528e5ad2969
aleksJoker <aleks.valushko@yandex.ru>
2020-02-25 22:59:06 +0300

    Updates

---
+++ precache-manifest.a9b8f1cd340576781b656a81837352f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2399fafc8ab5c8bf27872a6f08c04e0d",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "79a320622b0b6b890e52",
+    "url": "/todolist/static/css/main.7cdb31b5.chunk.css"
+  },
+  {
+    "revision": "322f8f58b9286e1c2960",
+    "url": "/todolist/static/js/2.7c0ba429.chunk.js"
+  },
+  {
+    "revision": "79a320622b0b6b890e52",
+    "url": "/todolist/static/js/main.30e68421.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  },
+  {
+    "revision": "491ecf8094a4c5acd47c27cfdaeb6e81",
+    "url": "/todolist/static/media/basket.491ecf80.svg"
+  }
+]);
